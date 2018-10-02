5548bee9ded385bfa8568fcf6abf83ff6eb330cc
experiments-albererre <38617665+experiments-albererre@users.noreply.github.com>
2018-10-02 18:00:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ * Hola
+ * @param {string} expression
+ * @returns {int} suma
+ * @description crea un portal estelar
+ * @author papito
+ */
+function sumNumbersFrom(expression) {
+
+    const separatorPattern = '//';
+    const endSeparatorCommand = ';';
+    const defaultSeparator = ',';
+
+    function splitBy(expression, separator) {
+        return expression.split(separator);
+    }
+
+    function getSeparator(separatorPattern, expression) {
+        const configuration = splitBy(expression, endSeparatorCommand)[0]
+        const indexSeparator = configuration.indexOf(separatorPattern);
+        return configuration.substring(indexSeparator + separatorPattern.length);
+    }
+
+    function computeResultFrom(numbers) {
+        let result = 0;
+        numbers.map(number => {
+            if (!isNaN(number)) {
+                result += Number(number);
+            }
+        })
+        return result;
+    }
+
+    function hasSpecialSeparator(expression) {
+        return expression.startsWith(separatorPattern)
+    }
+
+    function calculate (expression){
+        let separator = defaultSeparator;
+        if (hasSpecialSeparator(expression)) {
+            separator = getSeparator(separatorPattern, expression)
+            expression = splitBy(expression, endSeparatorCommand)[1]
+        }
+        const numbers = splitBy(expression, separator)
+        return computeResultFrom(numbers);
+    }
+
+    return calculate(expression);
+}
+
+module.exports = sumNumbersFrom;
